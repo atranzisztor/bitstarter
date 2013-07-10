bebce85a7fc02f9ce9d96c931ed16fda2acbdeeb
Zsiga Csaba <atranzisztor@gmail.com>
2013-07-10 21:22:31 +0000

    hello from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+var Buffer = require('buffer');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var s = fs.readFile("index.html");
+  var buffer = new Buffer(s, "utf-8")
+  response.send(buffer.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;

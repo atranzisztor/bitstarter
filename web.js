e9b5e8bf58edf3d2f63605525c45f4f236c14625
Zsiga Csaba <atranzisztor@gmail.com>
2013-07-10 21:39:07 +0000

    fix 3

--- web.js
+++ web.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var fs = require('fs');
-//var Buffer = require('buffer');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var s = fs.readFile("index.html");
-  var buffer = new Buffer(s, "utf-8");
-  response.send(buffer.toString("utf-8"));
+  var buff = fs.readFile("index.html");
+  response.send(buff.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
